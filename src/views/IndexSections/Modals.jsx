8eddbd9bc5ac9f516ef77ce73efa6dc9c6bb1ea9
Manu <emanuelnazare@yahoo.ro>
2019-05-06 18:38:27 +0300

    - Modals done

--- src/views/IndexSections/Modals.jsx
+++ src/views/IndexSections/Modals.jsx
@@ -18,6 +18,14 @@ import {
 } from "reactstrap";
 
 class Modals extends React.Component {
+  state = {
+    defaultModal: false
+  };
+  toggleModal = state => {
+    this.setState({
+      [state]: !this.state[state]
+    });
+  };
   render() {
     return (
       <>
@@ -25,208 +33,228 @@ class Modals extends React.Component {
           <span>Javascript Components</span>
         </h2>
         <h3 className="h4 text-success font-weight-bold mb-4">Modals</h3>
-        <Row>
-          <Col md="4">
-            <Button
-              block
-              className="mb-3"
-              color="primary"
-              data-target="#modal-default"
-              data-toggle="modal"
-              type="button"
-            >
-              Default
-            </Button>
-            <Modal>
-              <div className="modal-header">
-                <h6 className="modal-title" id="modal-title-default">
-                  Type your modal title
-                </h6>
-                <button
-                  aria-label="Close"
-                  className="close"
-                  data-dismiss="modal"
-                  type="button"
-                >
-                  <span aria-hidden={true}>×</span>
-                </button>
-              </div>
-              <div className="modal-body">
-                <p>
-                  Far far away, behind the word mountains, far from the
-                  countries Vokalia and Consonantia, there live the blind texts.
-                  Separated they live in Bookmarksgrove right at the coast of
-                  the Semantics, a large language ocean.
-                </p>
-                <p>
-                  A small river named Duden flows by their place and supplies it
-                  with the necessary regelialia. It is a paradisematic country,
-                  in which roasted parts of sentences fly into your mouth.
-                </p>
-              </div>
-              <div className="modal-footer">
-                <Button color="primary" type="button">
-                  Save changes
-                </Button>
-                <Button
-                  className="ml-auto"
-                  color="link"
-                  data-dismiss="modal"
-                  type="button"
-                >
-                  Close
-                </Button>
-              </div>
-            </Modal>
-          </Col>
-          <Col md="4">
-            <Button
-              block
-              className="mb-3"
-              color="warning"
-              data-target="#modal-notification"
-              data-toggle="modal"
-              type="button"
-            >
-              Notification
-            </Button>
-            <Modal contentClassName="bg-gradient-danger">
-              <div className="modal-header">
-                <h6 className="modal-title" id="modal-title-notification">
-                  Your attention is required
-                </h6>
-                <button
-                  aria-label="Close"
-                  className="close"
-                  data-dismiss="modal"
-                  type="button"
-                >
-                  <span aria-hidden={true}>×</span>
-                </button>
-              </div>
-              <div className="modal-body">
-                <div className="py-3 text-center">
-                  <i className="ni ni-bell-55 ni-3x" />
-                  <h4 className="heading mt-4">You should read this!</h4>
+          <Row>
+            <Col md="4">
+              <Button
+                block
+                className="mb-3"
+                color="primary"
+                type="button"
+                onClick={() => this.toggleModal("defaultModal")}
+              >
+                Default
+              </Button>
+              <Modal
+                className="modal-dialog-centered"
+                isOpen={this.state.defaultModal}
+                toggle={() => this.toggleModal("defaultModal")}
+              >
+                <div className="modal-header">
+                  <h6 className="modal-title" id="modal-title-default">
+                    Type your modal title
+                  </h6>
+                  <button
+                    aria-label="Close"
+                    className="close"
+                    data-dismiss="modal"
+                    type="button"
+                    onClick={() => this.toggleModal("defaultModal")}
+                  >
+                    <span aria-hidden={true}>×</span>
+                  </button>
+                </div>
+                <div className="modal-body">
+                  <p>
+                    Far far away, behind the word mountains, far from the
+                    countries Vokalia and Consonantia, there live the blind
+                    texts. Separated they live in Bookmarksgrove right at the
+                    coast of the Semantics, a large language ocean.
+                  </p>
                   <p>
                     A small river named Duden flows by their place and supplies
-                    it with the necessary regelialia.
+                    it with the necessary regelialia. It is a paradisematic
+                    country, in which roasted parts of sentences fly into your
+                    mouth.
                   </p>
                 </div>
-              </div>
-              <div className="modal-footer">
-                <Button className="btn-white" color="default" type="button">
-                  Ok, Got it
-                </Button>
-                <Button
-                  className="text-white ml-auto"
-                  color="link"
-                  data-dismiss="modal"
-                  type="button"
-                >
-                  Close
-                </Button>
-              </div>
-            </Modal>
-          </Col>
-          <Col md="4">
-            <Button
-              block
-              color="default"
-              data-target="#modal-form"
-              data-toggle="modal"
-              type="button"
-            >
-              Form
-            </Button>
-            <Modal>
-              <div className="modal-body p-0">
-                <Card className="bg-secondary shadow border-0">
-                  <CardHeader className="bg-white pb-5">
-                    <div className="text-muted text-center mb-3">
-                      <small>Sign in with</small>
-                    </div>
-                    <div className="btn-wrapper text-center">
-                      <Button
-                        className="btn-neutral btn-icon"
-                        color="default"
-                        href="#pablo"
-                        onClick={e => e.preventDefault()}
-                      >
-                        <span className="btn-inner--icon">
-                          <img
-                            alt="..."
-                            src={require("assets/img/icons/common/github.svg")}
-                          />
-                        </span>
-                        <span className="btn-inner--text">Github</span>
-                      </Button>
-                      <Button
-                        className="btn-neutral btn-icon"
-                        color="default"
-                        href="#pablo"
-                        onClick={e => e.preventDefault()}
-                      >
-                        <span className="btn-inner--icon">
-                          <img
-                            alt="..."
-                            src={require("assets/img/icons/common/google.svg")}
-                          />
-                        </span>
-                        <span className="btn-inner--text">Google</span>
-                      </Button>
-                    </div>
-                  </CardHeader>
-                  <CardBody className="px-lg-5 py-lg-5">
-                    <div className="text-center text-muted mb-4">
-                      <small>Or sign in with credentials</small>
-                    </div>
-                    <Form role="form">
-                      <FormGroup className="mb-3">
-                        <InputGroup className="input-group-alternative">
-                          <InputGroupAddon addonType="prepend">
-                            <InputGroupText>
-                              <i className="ni ni-email-83" />
-                            </InputGroupText>
-                          </InputGroupAddon>
-                          <Input placeholder="Email" type="email" />
-                        </InputGroup>
-                      </FormGroup>
-                      <FormGroup>
-                        <InputGroup className="input-group-alternative">
-                          <InputGroupAddon addonType="prepend">
-                            <InputGroupText>
-                              <i className="ni ni-lock-circle-open" />
-                            </InputGroupText>
-                          </InputGroupAddon>
-                          <Input placeholder="Password" type="password" />
-                        </InputGroup>
-                      </FormGroup>
-                      <div className="custom-control custom-control-alternative custom-checkbox">
-                        <input
-                          className="custom-control-input"
-                          id=" customCheckLogin"
-                          type="checkbox"
-                        />
-                        <label
-                          className="custom-control-label"
-                          htmlFor=" customCheckLogin"
-                        >
-                          <span>Remember me</span>
-                        </label>
+                <div className="modal-footer">
+                  <Button color="primary" type="button">
+                    Save changes
+                  </Button>
+                  <Button
+                    className="ml-auto"
+                    color="link"
+                    data-dismiss="modal"
+                    type="button"
+                    onClick={() => this.toggleModal("defaultModal")}
+                  >
+                    Close
+                  </Button>
+                </div>
+              </Modal>
+            </Col>
+            <Col md="4">
+              <Button
+                block
+                className="mb-3"
+                color="warning"
+                type="button"
+                onClick={() => this.toggleModal("notificationModal")}
+              >
+                Notification
+              </Button>
+              <Modal
+                className="modal-dialog-centered modal-danger"
+                contentClassName="bg-gradient-danger"
+                isOpen={this.state.notificationModal}
+                toggle={() => this.toggleModal("notificationModal")}
+              >
+                <div className="modal-header">
+                  <h6 className="modal-title" id="modal-title-notification">
+                    Your attention is required
+                  </h6>
+                  <button
+                    aria-label="Close"
+                    className="close"
+                    data-dismiss="modal"
+                    type="button"
+                    onClick={() => this.toggleModal("notificationModal")}
+                  >
+                    <span aria-hidden={true}>×</span>
+                  </button>
+                </div>
+                <div className="modal-body">
+                  <div className="py-3 text-center">
+                    <i className="ni ni-bell-55 ni-3x" />
+                    <h4 className="heading mt-4">You should read this!</h4>
+                    <p>
+                      A small river named Duden flows by their place and
+                      supplies it with the necessary regelialia.
+                    </p>
+                  </div>
+                </div>
+                <div className="modal-footer">
+                  <Button className="btn-white" color="default" type="button">
+                    Ok, Got it
+                  </Button>
+                  <Button
+                    className="text-white ml-auto"
+                    color="link"
+                    data-dismiss="modal"
+                    type="button"
+                    onClick={() => this.toggleModal("notificationModal")}
+                  >
+                    Close
+                  </Button>
+                </div>
+              </Modal>
+            </Col>
+            <Col md="4">
+              <Button
+                block
+                color="default"
+                type="button"
+                onClick={() => this.toggleModal("formModal")}
+              >
+                Form
+              </Button>
+              <Modal
+                className="modal-dialog-centered"
+                size="sm"
+                isOpen={this.state.formModal}
+                toggle={() => this.toggleModal("formModal")}
+              >
+                <div className="modal-body p-0">
+                  <Card className="bg-secondary shadow border-0">
+                    <CardHeader className="bg-transparent pb-5">
+                      <div className="text-muted text-center mt-2 mb-3">
+                        <small>Sign in with</small>
                       </div>
-                      <div className="text-center">
-                        <Button className="my-4" color="primary" type="button">
-                          Sign in
+                      <div className="btn-wrapper text-center">
+                        <Button
+                          className="btn-neutral btn-icon"
+                          color="default"
+                          href="#pablo"
+                          onClick={e => e.preventDefault()}
+                        >
+                          <span className="btn-inner--icon">
+                            <img
+                              alt="..."
+                              src={require("assets/img/icons/common/github.svg")}
+                            />
+                          </span>
+                          <span className="btn-inner--text">Github</span>
+                        </Button>
+                        <Button
+                          className="btn-neutral btn-icon"
+                          color="default"
+                          href="#pablo"
+                          onClick={e => e.preventDefault()}
+                        >
+                          <span className="btn-inner--icon">
+                            <img
+                              alt="..."
+                              src={require("assets/img/icons/common/google.svg")}
+                            />
+                          </span>
+                          <span className="btn-inner--text">Google</span>
                         </Button>
                       </div>
-                    </Form>
-                  </CardBody>
-                </Card>
-              </div>
-            </Modal>
-          </Col>
-        </Row>
+                    </CardHeader>
+                    <CardBody className="px-lg-5 py-lg-5">
+                      <div className="text-center text-muted mb-4">
+                        <small>Or sign in with credentials</small>
+                      </div>
+                      <Form role="form">
+                        <FormGroup className="mb-3">
+                          <InputGroup className="input-group-alternative">
+                            <InputGroupAddon addonType="prepend">
+                              <InputGroupText>
+                                <i className="ni ni-email-83" />
+                              </InputGroupText>
+                            </InputGroupAddon>
+                            <Input placeholder="Email" type="email" />
+                          </InputGroup>
+                        </FormGroup>
+                        <FormGroup>
+                          <InputGroup className="input-group-alternative">
+                            <InputGroupAddon addonType="prepend">
+                              <InputGroupText>
+                                <i className="ni ni-lock-circle-open" />
+                              </InputGroupText>
+                            </InputGroupAddon>
+                            <Input placeholder="Password" type="password" />
+                          </InputGroup>
+                        </FormGroup>
+                        <div className="custom-control custom-control-alternative custom-checkbox">
+                          <input
+                            className="custom-control-input"
+                            id=" customCheckLogin"
+                            type="checkbox"
+                          />
+                          <label
+                            className="custom-control-label"
+                            htmlFor=" customCheckLogin"
+                          >
+                            <span className="text-muted">Remember me</span>
+                          </label>
+                        </div>
+                        <div className="text-center">
+                          <Button
+                            className="my-4"
+                            color="primary"
+                            type="button"
+                          >
+                            Sign in
+                          </Button>
+                        </div>
+                      </Form>
+                    </CardBody>
+                  </Card>
+                </div>
+              </Modal>
+            </Col>
+          </Row>
       </>
     );
   }
