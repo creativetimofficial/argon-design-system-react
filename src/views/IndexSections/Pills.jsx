188b4a8a6fecbc643f3dbcb4f126b87383675010
Manu <emanuelnazare@yahoo.ro>
2019-05-07 10:44:48 +0300

    - Functionality for Pills section

--- src/views/IndexSections/Pills.jsx
+++ src/views/IndexSections/Pills.jsx
@@ -1,9 +1,18 @@
 import React from "react";
-
+import classnames from "classnames";
 // reactstrap components
 import { NavItem, NavLink, Nav, Col } from "reactstrap";
 
 class Index extends React.Component {
+  state = {
+    circledNavPills: 1
+  };
+  toggleNavs = (e, state, index) => {
+    e.preventDefault();
+    this.setState({
+      [state]: index
+    });
+  };
   render() {
     return (
       <>
@@ -11,59 +20,53 @@ class Index extends React.Component {
           <h3 className="h4 text-success font-weight-bold mb-5">
             Navigation Pills
           </h3>
-          <Nav
-            className="nav-pills-circle mb-3"
-            id="tabs_2"
-            pills
-            role="tablist"
-          >
-            <NavItem>
-              <NavLink
-                aria-controls="home"
-                aria-selected={true}
-                className="rounded-circle active"
-                data-toggle="tab"
-                href="#pablo"
-                id="home-tab"
-                onClick={e => e.preventDefault()}
-                role="tab"
-              >
-                <span className="nav-link-icon d-block">
-                  <i className="ni ni-atom" />
-                </span>
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink
-                aria-controls="profile"
-                aria-selected={false}
-                data-toggle="tab"
-                href="#pablo"
-                id="profile-tab"
-                onClick={e => e.preventDefault()}
-                role="tab"
-              >
-                <span className="nav-link-icon d-block">
-                  <i className="ni ni-chat-round" />
-                </span>
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink
-                aria-controls="contact"
-                aria-selected={false}
-                data-toggle="tab"
-                href="#pablo"
-                id="contact-tab"
-                onClick={e => e.preventDefault()}
-                role="tab"
-              >
-                <span className="nav-link-icon d-block">
-                  <i className="ni ni-cloud-download-95" />
-                </span>
-              </NavLink>
-            </NavItem>
-          </Nav>
+          <Nav className="nav-pills-circle" id="tabs_2" pills role="tablist">
+          <NavItem>
+            <NavLink
+              aria-selected={this.state.circledNavPills === 1}
+              className={classnames("rounded-circle", {
+                active: this.state.circledNavPills === 1
+              })}
+              onClick={e => this.toggleNavs(e, "circledNavPills", 1)}
+              href="#pablo"
+              role="tab"
+            >
+              <span className="nav-link-icon d-block">
+                <i className="ni ni-atom" />
+              </span>
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              aria-selected={this.state.circledNavPills === 2}
+              className={classnames("rounded-circle", {
+                active: this.state.circledNavPills === 2
+              })}
+              onClick={e => this.toggleNavs(e, "circledNavPills", 2)}
+              href="#pablo"
+              role="tab"
+            >
+              <span className="nav-link-icon d-block">
+                <i className="ni ni-chat-round" />
+              </span>
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              aria-selected={this.state.circledNavPills === 3}
+              className={classnames("rounded-circle", {
+                active: this.state.circledNavPills === 3
+              })}
+              onClick={e => this.toggleNavs(e, "circledNavPills", 3)}
+              href="#pablo"
+              role="tab"
+            >
+              <span className="nav-link-icon d-block">
+                <i className="ni ni-cloud-download-95" />
+              </span>
+            </NavLink>
+          </NavItem>
+        </Nav>
         </Col>
       </>
     );
