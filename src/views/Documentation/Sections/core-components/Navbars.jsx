d714168558946f9d2f4f2b931d1d7bb3b6704972
Manu <emanuelnazare@yahoo.ro>
2019-05-07 18:02:49 +0300

    - docs done

--- src/views/Documentation/Sections/core-components/Navbars.jsx
+++ src/views/Documentation/Sections/core-components/Navbars.jsx
@@ -11,42 +11,22 @@ class Navbar extends React.Component {
           <div className="avatar-group mt-3" />
         </div>
         <p className="ct-lead">
-          In our template product, we have two demo navbars that can be found
-          inside{" "}
+          In our template product, we have just one demo navbar that is used for
+          all the present pages. This one is found inside:{" "}
           <code className="highlighter-rouge">
-            src/components/Navbars/AdminNavbar.jsx
-          </code>{" "}
-          and{" "}
-          <code className="highlighter-rouge">
-            src/components/Navbars/AuthNavbar.jsx
+            src/components/Navbars/DemoNavbar.jsx
           </code>
           .
         </p>
-        <h2 id="content">
-          <code className="highlighter-rouge">
-            src/components/Navbars/AdminNavbar.jsx
-          </code>
-        </h2>
         <p>
-          It is used to create the navbar / header that appears on the{" "}
-          <code className="highlighter-rouge">src/layouts/Admin.jsx</code>.
+          It is diveded in two parts, the left one, that is used for logo and
+          links inside the app, and the right part with some social buttons and
+          a download one.
         </p>
         <p>
-          It is diveded in two parts, the left one, that is used for the page
-          name, and the right part with a search and a user.
-        </p>
-        <h2 id="content">
-          <code className="highlighter-rouge">
-            src/components/Navbars/AuthNavbar.jsx
-          </code>
-        </h2>
-        <p>
-          It is used to create the navbar / header that appears on the{" "}
-          <code className="highlighter-rouge">src/layouts/Auth.jsx</code>.
-        </p>
-        <p>
-          It is diveded in two parts, the left one, that is used for the product
-          logo, and the right part with some links to navigate inside the app.
+          This component does not accept any props. So everything in these two
+          components is static. Feel free to change them as you wish. Just
+          import them, and render them like normal HTML tags.
         </p>
       </>
     );
