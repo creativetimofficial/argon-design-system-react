acd1056f352d3a025aa9f7aafb2009c62034d6eb
Manu <emanuelnazare@yahoo.ro>
2019-05-07 17:26:48 +0300

    - Forms docs is done

--- src/views/Documentation/Sections/restyled-components/Forms.jsx
+++ src/views/Documentation/Sections/restyled-components/Forms.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { prism } from "react-syntax-highlighter/dist/styles/prism";
+// plugin that creates slider
+import Slider from "nouislider";
 // reactstrap components
 import {
   FormGroup,
@@ -345,77 +347,6 @@ class Forms extends React.Component {
 export default Forms;
 `;
 
-const codeAlternative3 = `import React from "react";
-
-
-// reactstrap components
-import {
-  FormGroup,
-  Form,
-  Input,
-  InputGroupAddon,
-  InputGroupText,
-  InputGroup,
-  Row,
-  Col
-} from "reactstrap";
-
-class Forms extends React.Component {
-  render() {
-    return (
-      <>
-        <div className="custom-control custom-control-alternative custom-checkbox mb-3">
-          <input
-            className="custom-control-input"
-            id="customCheck5"
-            type="checkbox"
-          />
-          <label className="custom-control-label" htmlFor="customCheck5">
-            Unchecked
-          </label>
-        </div>
-        <div className="custom-control custom-control-alternative custom-checkbox mb-3">
-          <input
-            className="custom-control-input"
-            defaultChecked
-            id="customCheck6"
-            type="checkbox"
-          />
-          <label className="custom-control-label" htmlFor="customCheck6">
-            Checked
-          </label>
-        </div>
-        <div className="custom-control custom-control-alternative custom-checkbox mb-3">
-          <input
-            className="custom-control-input"
-            disabled
-            id="customCheck7"
-            type="checkbox"
-          />
-          <label className="custom-control-label" htmlFor="customCheck7">
-            Disabled Unchecked
-          </label>
-        </div>
-        <div className="custom-control custom-control-alternative custom-checkbox mb-3">
-          <input
-            className="custom-control-input"
-            defaultChecked
-            disabled
-            id="customCheck8"
-            type="checkbox"
-          />
-          <label className="custom-control-label" htmlFor="customCheck8">
-            Disabled Checked
-          </label>
-        </div>
-      </>
-    );
-  }
-}
-
-export default Forms;
-`;
-
 const codeRadioButtons = `import React from "react";
 
 
@@ -491,115 +422,6 @@ class Forms extends React.Component {
 export default Forms;
 `;
 
-const codeAlternative4 = `import React from "react";
-
-
-// reactstrap components
-import {
-  FormGroup,
-  Form,
-  Input,
-  InputGroupAddon,
-  InputGroupText,
-  InputGroup,
-  Row,
-  Col
-} from "reactstrap";
-
-class Forms extends React.Component {
-  render() {
-    return (
-      <>
-        <div className="custom-control custom-control-alternative custom-radio mb-3">
-          <input
-            className="custom-control-input"
-            id="customRadio1"
-            name="custom-radio-1"
-            type="radio"
-          />
-          <label className="custom-control-label" htmlFor="customRadio1">
-            Unchecked
-          </label>
-        </div>
-        <div className="custom-control custom-control-alternative custom-radio mb-3">
-          <input
-            className="custom-control-input"
-            defaultChecked
-            id="customRadio2"
-            name="custom-radio-1"
-            type="radio"
-          />
-          <label className="custom-control-label" htmlFor="customRadio2">
-            Checked
-          </label>
-        </div>
-        <div className="custom-control custom-control-alternative custom-radio mb-3">
-          <input
-            className="custom-control-input"
-            disabled
-            id="customRadio3"
-            name="custom-radio-1"
-            type="radio"
-          />
-          <label className="custom-control-label" htmlFor="customRadio3">
-            Disabled unchecked
-          </label>
-        </div>
-        <div className="custom-control custom-control-alternative custom-radio mb-3">
-          <input
-            className="custom-control-input"
-            defaultChecked
-            disabled
-            id="customRadio4"
-            name="custom-radio-1"
-            type="radio"
-          />
-          <label className="custom-control-label" htmlFor="customRadio4">
-            Disabled checkbox
-          </label>
-        </div>
-      </>
-    );
-  }
-}
-
-export default Forms;
-import React from "react";
-
-
-// reactstrap components
-import {
-  FormGroup,
-  Form,
-  Input,
-  InputGroupAddon,
-  InputGroupText,
-  InputGroup,
-  Row,
-  Col
-} from "reactstrap";
-
-class Forms extends React.Component {
-  render() {
-    return (
-      <>
-        <label className="custom-toggle">
-          <input type="checkbox" />
-          <span className="custom-toggle-slider rounded-circle" />
-        </label>
-        <span className="clearfix" />
-        <label className="custom-toggle">
-          <input defaultChecked type="checkbox" />
-          <span className="custom-toggle-slider rounded-circle" />
-        </label>
-      </>
-    );
-  }
-}
-
-export default Forms;
-`;
-
 const codeToggleButtons = `import React from "react";
 
 
@@ -636,9 +458,137 @@ class Forms extends React.Component {
 export default Forms;
 `;
 
-const codeSliders = ``;
+const codeSliders = `import React from "react";
+// plugin that creates slider
+import Slider from "nouislider";
+// reactstrap components
+import {
+  Row,
+  Col
+} from "reactstrap";
+
+// reactstrap components
+import {
+  Row,
+  Col
+} from "reactstrap";
 
 class Forms extends React.Component {
+  state = {
+    simpleValue: 100.0,
+    rangeLow: 200.0,
+    rangeHigh: 400.0
+  };
+  componentDidMount() {
+    // slider1 init
+    var slider1 = this.refs.slider1;
+    Slider.create(slider1, {
+      start: [0.0],
+      connect: [true, false],
+      step: 0.01,
+      range: { min: 100.0, max: 500.0 }
+    }).on(
+      "update",
+      function(values, handle) {
+        this.setState({ simpleValue: values[0] });
+      }.bind(this)
+    );
+
+    // slider2 init
+    var slider2 = this.refs.slider2;
+    Slider.create(slider2, {
+      start: [200.0, 400.0],
+      connect: [false, true, false],
+      step: 0.01,
+      range: { min: 100.0, max: 500.0 }
+    }).on(
+      "update",
+      function(values, handle) {
+        this.setState({ rangeLow: values[0], rangeHigh: values[1] });
+      }.bind(this)
+    );
+  }
+  render() {
+    return (
+      <>
+        <Row>
+          <Col className="mt-4 mt-md-0">
+            <div className="mb-3">
+              <small className="text-uppercase font-weight-bold">Sliders</small>
+            </div>
+            {/* Simple slider */}
+            <div className="input-slider-container">
+              <div className="slider" ref="slider1" />
+              <Row className="mt-3 d-none">
+                <Col xs="6">
+                  <span className="range-slider-value">
+                    {this.state.simpleValue}
+                  </span>
+                </Col>
+              </Row>
+            </div>
+            {/* Range slider */}
+            <div className="mt-5">
+              {/* Range slider container */}
+              <div className="slider" ref="slider2" />
+              <Row className="d-none">
+                <Col xs="6">
+                  <span className="range-slider-value value-low">
+                    {this.state.rangeLow}
+                  </span>
+                </Col>
+                <Col className="text-right" xs="6">
+                  <span className="range-slider-value value-high">
+                    {this.state.rangeHigh}
+                  </span>
+                </Col>
+              </Row>
+            </div>
+          </Col>
+        </Row>
+      </>
+    );
+  }
+}
+
+export default Forms;
+`;
+
+class Forms extends React.Component {
+  state = {
+    simpleValue: 100.0,
+    rangeLow: 200.0,
+    rangeHigh: 400.0
+  };
+  componentDidMount() {
+    // slider1 init
+    var slider1 = this.refs.slider1;
+    Slider.create(slider1, {
+      start: [0.0],
+      connect: [true, false],
+      step: 0.01,
+      range: { min: 100.0, max: 500.0 }
+    }).on(
+      "update",
+      function(values, handle) {
+        this.setState({ simpleValue: values[0] });
+      }.bind(this)
+    );
+
+    // slider2 init
+    var slider2 = this.refs.slider2;
+    Slider.create(slider2, {
+      start: [200.0, 400.0],
+      connect: [false, true, false],
+      step: 0.01,
+      range: { min: 100.0, max: 500.0 }
+    }).on(
+      "update",
+      function(values, handle) {
+        this.setState({ rangeLow: values[0], rangeHigh: values[1] });
+      }.bind(this)
+    );
+  }
   render() {
     return (
       <>
@@ -881,56 +831,6 @@ class Forms extends React.Component {
         <SyntaxHighlighter language="jsx" style={prism}>
           {codeCheckboxes}
         </SyntaxHighlighter>
-        <h3 id="alternative-2">Alternative</h3>
-        <div className="ct-example">
-          <div className="custom-control custom-control-alternative custom-checkbox mb-3">
-            <input
-              className="custom-control-input"
-              id="customCheck5"
-              type="checkbox"
-            />
-            <label className="custom-control-label" htmlFor="customCheck5">
-              Unchecked
-            </label>
-          </div>
-          <div className="custom-control custom-control-alternative custom-checkbox mb-3">
-            <input
-              className="custom-control-input"
-              defaultChecked
-              id="customCheck6"
-              type="checkbox"
-            />
-            <label className="custom-control-label" htmlFor="customCheck6">
-              Checked
-            </label>
-          </div>
-          <div className="custom-control custom-control-alternative custom-checkbox mb-3">
-            <input
-              className="custom-control-input"
-              disabled
-              id="customCheck7"
-              type="checkbox"
-            />
-            <label className="custom-control-label" htmlFor="customCheck7">
-              Disabled Unchecked
-            </label>
-          </div>
-          <div className="custom-control custom-control-alternative custom-checkbox mb-3">
-            <input
-              className="custom-control-input"
-              defaultChecked
-              disabled
-              id="customCheck8"
-              type="checkbox"
-            />
-            <label className="custom-control-label" htmlFor="customCheck8">
-              Disabled Checked
-            </label>
-          </div>
-        </div>
-        <SyntaxHighlighter language="jsx" style={prism}>
-          {codeAlternative3}
-        </SyntaxHighlighter>
         <h2 id="radio-buttons">Radio buttons</h2>
         <div className="ct-example">
           <div className="custom-control custom-radio mb-3">
@@ -985,60 +885,6 @@ class Forms extends React.Component {
         <SyntaxHighlighter language="jsx" style={prism}>
           {codeRadioButtons}
         </SyntaxHighlighter>
-        <h3 id="alternative-3">Alternative</h3>
-        <div className="ct-example">
-          <div className="custom-control custom-control-alternative custom-radio mb-3">
-            <input
-              className="custom-control-input"
-              id="customRadio1"
-              name="custom-radio-1"
-              type="radio"
-            />
-            <label className="custom-control-label" htmlFor="customRadio1">
-              Unchecked
-            </label>
-          </div>
-          <div className="custom-control custom-control-alternative custom-radio mb-3">
-            <input
-              className="custom-control-input"
-              defaultChecked
-              id="customRadio2"
-              name="custom-radio-1"
-              type="radio"
-            />
-            <label className="custom-control-label" htmlFor="customRadio2">
-              Checked
-            </label>
-          </div>
-          <div className="custom-control custom-control-alternative custom-radio mb-3">
-            <input
-              className="custom-control-input"
-              disabled
-              id="customRadio3"
-              name="custom-radio-1"
-              type="radio"
-            />
-            <label className="custom-control-label" htmlFor="customRadio3">
-              Disabled unchecked
-            </label>
-          </div>
-          <div className="custom-control custom-control-alternative custom-radio mb-3">
-            <input
-              className="custom-control-input"
-              defaultChecked
-              disabled
-              id="customRadio4"
-              name="custom-radio-1"
-              type="radio"
-            />
-            <label className="custom-control-label" htmlFor="customRadio4">
-              Disabled checkbox
-            </label>
-          </div>
-        </div>
-        <SyntaxHighlighter language="jsx" style={prism}>
-          {codeAlternative4}
-        </SyntaxHighlighter>
         <h2 id="toggle-buttons">Toggle buttons</h2>
         <div className="ct-example">
           <label className="custom-toggle">
@@ -1056,50 +902,43 @@ class Forms extends React.Component {
         </SyntaxHighlighter>
         <h2 id="sliders">Sliders</h2>
         <div className="ct-example">
-          {/* Simple slider */}
-          <div className="input-slider-container">
-            <div
-              className="input-slider"
-              data-range-value-max="500"
-              data-range-value-min="100"
-              id="input-slider"
-            />
-            {/* Input slider values */}
-            <Row className="mt-3 d-none">
-              <Col xs="6">
-                <span
-                  className="range-slider-value"
-                  data-range-value-low="100"
-                  id="input-slider-value"
-                />
-              </Col>
-            </Row>
-          </div>
-          <div className="mt-5">
-            {/* Range slider container */}
-            <div
-              data-range-value-max="500"
-              data-range-value-min="100"
-              id="input-slider-range"
-            />
-            {/* Range slider values */}
-            <Row className="d-none">
-              <Col xs="6">
-                <span
-                  className="range-slider-value value-low"
-                  data-range-value-low="200"
-                  id="input-slider-range-value-low"
-                />
-              </Col>
-              <Col className="text-right" xs="6">
-                <span
-                  className="range-slider-value value-high"
-                  data-range-value-high="400"
-                  id="input-slider-range-value-high"
-                />
-              </Col>
-            </Row>
-          </div>
+          <Row>
+            <Col className="mt-4 mt-md-0">
+              <div className="mb-3">
+                <small className="text-uppercase font-weight-bold">
+                  Sliders
+                </small>
+              </div>
+              {/* Simple slider */}
+              <div className="input-slider-container">
+                <div className="slider" ref="slider1" />
+                <Row className="mt-3 d-none">
+                  <Col xs="6">
+                    <span className="range-slider-value">
+                      {this.state.simpleValue}
+                    </span>
+                  </Col>
+                </Row>
+              </div>
+              {/* Range slider */}
+              <div className="mt-5">
+                {/* Range slider container */}
+                <div className="slider" ref="slider2" />
+                <Row className="d-none">
+                  <Col xs="6">
+                    <span className="range-slider-value value-low">
+                      {this.state.rangeLow}
+                    </span>
+                  </Col>
+                  <Col className="text-right" xs="6">
+                    <span className="range-slider-value value-high">
+                      {this.state.rangeHigh}
+                    </span>
+                  </Col>
+                </Row>
+              </div>
+            </Col>
+          </Row>
         </div>
         <SyntaxHighlighter language="jsx" style={prism}>
           {codeSliders}
