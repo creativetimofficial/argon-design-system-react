8e15ae4ae8d4fab0475352eae5f43e4f4199246b
Manu <emanuelnazare@yahoo.ro>
2019-07-05 09:22:46 +0300

    - added copyright

--- src/components/Footers/SimpleFooter.jsx
+++ src/components/Footers/SimpleFooter.jsx
@@ -1,3 +1,20 @@
+/*!
+
+=========================================================
+* Argon Design System React - v1.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
 /*eslint-disable*/
 import React from "react";
 // reactstrap components
