6b0d9d27734d0a54492cfc21cddbbbb5c2183888
EINazare <emanuelioannazare@gmail.com>
2020-02-14 16:11:37 +0200

    docs: add ?v1.1.0

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import "assets/vendor/nucleo/css/nucleo.css";
 import "assets/vendor/font-awesome/css/font-awesome.min.css";
-import "assets/scss/argon-design-system-react.scss";
+import "assets/scss/argon-design-system-react.scss?v1.1.0";
 
 import Index from "views/Index.js";
 import Landing from "views/examples/Landing.js";
